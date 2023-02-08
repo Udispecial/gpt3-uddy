a5153ca24b6f237e6b53422ba165b907277bbd9a
Uduak Edet <edet.uduak@techvibesltd.com>
2023-02-08 09:12:51 +0100

    first commit

---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+import Blog from "./blog/Blog";
+import Features from "./features/Features";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Possibilities from "./possibilities/Possibilities";
+import WhatGPT3 from "./whatGPT3/WhatGPT3";
+
+export { Blog, Features, Header, Footer, Possibilities, WhatGPT3 };
